2e8d495dc42884b34b5467f8298295533467b9e2
Anuj Kumar Sharma <anujitm2007@gmail.com>
2021-09-13 08:13:20 +0000

    Updating logs in app.py and fixed typo in 404.html

--- techtrends/app.py
+++ techtrends/app.py
@@ -1,35 +1,72 @@
+import logging
 import sqlite3
+import sys
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
-from datetime import datetime
-import logging
 
-# Count all database connections
-connection_count = 0
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
-    global connection_count
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
-    connection_count += 1
     return connection
+
 
 # Function to get a post using its ID
 def get_post(post_id):
     connection = get_db_connection()
     post = connection.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
+                              (post_id,)).fetchone()
     connection.close()
     return post
+
+
+def get_article_count(metrics_obj):
+    """
+    Count the number of articles and increment the number of connections used
+
+    Parameters:
+    metrics_obj (dict): Dictionary with basic data for metrics endpoint response
+    """
+    connection = get_db_connection()
+    article_count = connection.execute('SELECT count(*) FROM posts').fetchone()
+    connection.close()
+
+    metrics_obj['db_connection_count'] += 1
+    metrics_obj['post_count'] = article_count[0]
+
+
+def valid_db_connection():
+    """
+    Checks if connecting to database is successful.
+    """
+    try:
+        connection = get_db_connection()
+        connection.close()
+    except:
+        raise Exception("Database connection failure")
+
+
+def post_table_exists():
+    """
+    Checks if POST table exists.
+    """
+    try:
+        connection = get_db_connection()
+        connection.execute('SELECT 1 FROM posts').fetchone()
+        connection.close()
+    except:
+        raise Exception("Table 'posts' does not exist")
+
 
 # Define the Flask application
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
-# Define the main route of the web application 
+
+# Define the main route of the web application
 @app.route('/')
 def index():
     connection = get_db_connection()
@@ -37,22 +74,28 @@ def index():
     connection.close()
     return render_template('index.html', posts=posts)
 
-# Define how each individual article is rendered 
+
+# Define how each individual article is rendered
 # If the post ID is not found a 404 page is shown
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-      return render_template('404.html'), 404
+        logging.error('Article with id {} does not exists'.format(post_id))
+        return render_template('404.html'), 404
     else:
-      return render_template('post.html', post=post)
+        logging.info('Article "{}" retrieved!'.format(post['title']))
+        return render_template('post.html', post=post)
+
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    logging.info('"About Us" page was retrieved')
     return render_template('about.html')
 
-# Define the post creation functionality 
+
+# Define the post creation functionality
 @app.route('/create', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
@@ -64,44 +107,63 @@ def create():
         else:
             connection = get_db_connection()
             connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
+                               (title, content))
             connection.commit()
             connection.close()
+
+            logging.info('Article "{}" created'.format(title))
 
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
-# Define healthz endpoint
-@app.route('/healthz')
+
+@app.route('/healthz', methods=['GET'])
 def healthz():
+
+    response_body = {'result': 'OK - healthy'}
+    status_code = 200
+
     try:
-        connection = get_db_connection()
-        connection.cursor()
-        connection.execute('SELECT * FROM posts')
-        connection.close()
-        return {'result': 'OK - healthy'}
-    except Exception:
-        return {'result': 'ERROR - unhealthy'}, 500
+        valid_db_connection()
+        post_table_exists()
+    except Exception as exc:
+        response_body['result'] = 'ERROR - unhealthy'
+        response_body['details'] = str(exc)
+        status_code = 500
 
-# Define metrics endpoint
-@app.route('/metrics')
+    response = app.response_class(
+        response=json.dumps(response_body),
+        status=status_code,
+        mimetype='application/json')
+
+    return response
+
+
+@app.route('/metrics', methods=['GET'])
 def metrics():
-    connection = get_db_connection()
-    posts = connection.execute('SELECT * FROM posts').fetchall()
-    connection.close()
-    post_count = len(posts)
-    data = {"db_connection_count": connection_count, "post_count": post_count}
-    return data
+    metrics_obj = {
+        'db_connection_count': 0,
+        'post_count': None
+    }
 
-#Function that logs messages
-def log_message(msg):
-    app.logger.info('{time} | {message}'.format(
-        time=datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), message=msg))
+    get_article_count(metrics_obj)
+
+    response = app.response_class(
+        response=json.dumps(metrics_obj),
+        status=200,
+        mimetype='application/json')
+
+    return response
+
 
 # start the application on port 3111
 if __name__ == "__main__":
-    ## stream logs to a file
-    logging.basicConfig(level=logging.DEBUG)
 
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stderr_handler = logging.StreamHandler(sys.stderr)
+    handlers = [stderr_handler, stdout_handler]
+
+
+    logging.basicConfig(level=logging.DEBUG, handlers=handlers, format="’%(levelname)s: %(name)-2s - [%(asctime)s] - %(message)s’")
     app.run(host='0.0.0.0', port='3111')
